4b8074ffebce2f21b76523cb42e51a96f5302a8f
Alexis Comte <alexis.comte73@icloud.com>
2023-05-29 17:33:04 +0200

    add brand logo component

---
+++ src/components/brand/index.tsx
@@ -0,0 +1,15 @@
+import Logo from '@/assets/Logo'
+import React from 'react'
+
+function Brand() {
+  return (
+    <div className='flex items-center gap-3'>
+      <div className='h-[42px] w-[42px] rounded-full flex items-center justify-center bg-primary'>
+        <Logo className='text-white' />
+      </div>
+      <p className='font-bold text-[20px]'>TheBookPlace</p>
+    </div>
+  )
+}
+
+export default Brand
