bc23fef4499a89f9b83e9e0298b0425c2d310c8a
Alexis Comte <alexis.comte73@icloud.com>
2023-05-31 11:47:14 +0200

    ajout du header dans le layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Header from '@/components/ui/Header'
 import './globals.css'
 import { Roboto } from 'next/font/google'
 
@@ -15,7 +16,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={roboto.className}>{children}</body>
+      <body className={roboto.className}>
+        <Header />
+        {children}
+      </body>
     </html>
   )
 }
