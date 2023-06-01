25375119bfa1aa0143d1d3493e64436ff4402a98
Alexis Comte <alexis.comte73@icloud.com>
2023-06-01 16:19:44 +0200

    ajout des fake

---
+++ src/fake/book.ts
@@ -0,0 +1,116 @@
+export const bookInfo = {
+  id: 1,
+  title: 'The Psychology of Money',
+  author: 'Morgan Housel',
+  publicationDate: new Date('2020-09-08T00:00:00.000Z'),
+  pages: 256,
+  rate: 4.8,
+  price: 14.99,
+  imageUrl:
+    'https://images.unsplash.com/photo-1592496431122-2349e0fbc666?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1212&q=80',
+  description:
+    'Doing well with money isn’t necessarily about what you know. It’s about how you behave. And behavior is hard to teach, even to really smart people. Money―investing, personal finance, and business decisions―is typically taught as a math-based field, where data and formulas tell us exactly what to do. But in the real world people don’t make financial decisions on a spreadsheet. They make them at the dinner table, or in a meeting room, where personal history, your own unique view of the world, ego, pride, marketing, and odd incentives are scrambled together. In The Psychology of Money, award-winning author Morgan Housel shares 19 short stories exploring the strange ways people think about money and teaches you how to make better sense of one of life’s most important topics.',
+}
+
+export const fakeBookInfo = [
+  {
+    id: 1,
+    title: 'The Lord of the Rings',
+    author: 'J.R.R. Tolkien',
+    publishedDate: new Date('1954-07-29'),
+    price: 19.99,
+    rate: 4.5,
+    imageUrl:
+      'https://static.fnac-static.com/multimedia/FR/images_produits/FR/Fnac.com/ZoomPE/2/5/2/9780261103252/tsp20130902130459/The-Lord-of-the-Rings.jpg',
+  },
+  {
+    id: 2,
+    title: "Harry Potter and the Philosopher's Stone",
+    author: 'J.K. Rowling',
+    publishedDate: new Date('1997-06-26'),
+    price: 9.99,
+    rate: 4.8,
+    imageUrl:
+      'https://images-na.ssl-images-amazon.com/images/I/51UoqRAxwEL._SX331_BO1,204,203,200_.jpg',
+  },
+  {
+    id: 3,
+    title: 'The mind of a leader',
+    author: 'Kevin Anderson',
+    publishedDate: new Date('1954-07-29'),
+    price: 7.99,
+    rate: 3.2,
+    imageUrl:
+      'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/yellow-business-leadership-book-cover-design-template-dce2f5568638ad4643ccb9e725e5d6ff.jpg?ts=1637017516',
+  },
+  {
+    id: 4,
+    title: 'The Book Of Art',
+    author: 'Regina Phalange',
+    publishedDate: new Date('1997-06-26'),
+    price: 12.5,
+    rate: 4.8,
+    imageUrl:
+      'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/art-book-cover-design-template-34323b0f0734dccded21e0e3bebf004c_screen.jpg?ts=1637015198',
+  },
+  {
+    id: 5,
+    title: 'Psychology of Money',
+    author: 'Morgan Housel',
+    publishedDate: new Date('1954-07-29'),
+    price: 25.99,
+    rate: 4.5,
+    imageUrl:
+      'https://images.unsplash.com/photo-1592496431122-2349e0fbc666?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Ym9vayUyMGNvdmVyfGVufDB8fDB8fHww&w=1000&q=80',
+  },
+  {
+    id: 6,
+    title: 'A Million to One',
+    author: 'Tony Faggioli',
+    publishedDate: new Date('1997-06-26'),
+    price: 9.99,
+    rate: 4.8,
+    imageUrl:
+      'https://designforwriters.com/wp-content/uploads/2017/10/design-for-writers-book-cover-tf-2-a-million-to-one.jpg',
+  },
+  {
+    id: 7,
+    title: 'Percy Jackson and the Olympians: The Lightning Thief',
+    author: 'Rick Riordan',
+    publishedDate: new Date('1954-07-29'),
+    price: 19.99,
+    rate: 4.5,
+    imageUrl:
+      'https://readriordan.com/wp-content/uploads/2022/02/Lightning-Thief-flat-cover.png',
+  },
+  {
+    id: 8,
+    title: 'Harry Potter and the Goblet of Fire',
+    author: 'J.K. Rowling',
+    publishedDate: new Date('1997-06-26'),
+    price: 9.99,
+    rate: 4.8,
+    imageUrl:
+      'https://images.ctfassets.net/usf1vwtuqyxm/3d9kpFpwHyjACq8H3EU6ra/85673f9e660407e5e4481b1825968043/English_Harry_Potter_4_Epub_9781781105672.jpg',
+  },
+  {
+    id: 9,
+    title: 'Think outside the box',
+    author: 'Leah Helford',
+    publishedDate: new Date('1954-07-29'),
+    price: 19.99,
+    rate: 4.5,
+    imageUrl:
+      'https://cdn.cp.adobe.io/content/2/rendition/0cc96c63-4978-4166-aed2-e7ec6a8d8243/artwork/c4c65bd3-4b92-4447-93f0-1d045046fd5c/version/0/format/jpg/dimension/width/size/400',
+  },
+  {
+    id: 10,
+    title: 'Here Is Real Magic',
+    author: 'Nate Staniforth',
+    publishedDate: new Date('1997-06-26'),
+    price: 9.99,
+    rate: 4.8,
+    imageUrl:
+      'https://taleaway.com/wp-content/uploads/2020/01/best-book-cover-designs-2018-here-is-real-magic-nate-staniforth.jpg',
+  },
+]
