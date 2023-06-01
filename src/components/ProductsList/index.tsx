502dda9ed7e54759d8470be2c64c0aa239fb10ad
Alexis Comte <alexis.comte73@icloud.com>
2023-06-01 16:22:04 +0200

    fix products list

--- src/components/ProductsList/index.tsx
+++ src/components/ProductsList/index.tsx
@@ -6,7 +6,7 @@ const ProductsList = () => {
   return (
     <ul className='grid gap-4 sm:gap-y-8 md:grid-cols-3 sm:grid-cols-2 xl:max-w-[1200px] xl:grid-cols-4'>
       {products.map((product) => (
-        <li className='sm:justify-self-center h-full'>
+        <li key={product.id} className='sm:justify-self-center h-full'>
           <BookCard key={product.id} bookInfo={product} />
         </li>
       ))}
